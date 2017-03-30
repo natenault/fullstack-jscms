27661c9bffb7b50507bf4686fde16bcd5b02f9b3
Nate Nault <nate@nathannault.com>
2017-03-31 04:07:41 +0800

    Scaffold server components directory

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var parser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+// Set server port
+var port = process.env.PORT || 3000;
+
+// Logger
+app.use(morgan('dev'));
+
+// Body parser
+app.use(parser.urlencoded({extended: true}));
+app.use(parser.json());
+
+// Serve static assets
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Start server
+app.listen(port, function() {
+  console.log('Server now running on port: ' + port);
+});
