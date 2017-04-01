dea81925fe168bb2c7c43589d0d6aa752e502995
Nate Nault <nate@nathannault.com>
2017-04-01 13:25:56 +0800

    Add blank JS files for app component controllers

---
+++ client/app/components/showPosts.js
@@ -0,0 +1,15 @@
+angular.module('jscmsApp.showposts', [])
+.controller('ShowPostsCtrl', function($scope, DataService) {
+
+  var initPostList = function() {
+    DataService.getAllPosts()
+    .then(function(posts) {
+      $scope.posts = posts;
+    })
+    .catch(function(error) {
+      coneole.log(error);
+    });
+  };
+  initPostList();
+
+});
