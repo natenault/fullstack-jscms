718ebe7835868d891ac6cf9df2b107bf2150a4a0
Nate Nault <nate@nathannault.com>
2017-04-04 08:20:23 +0800

    Remove Gulpfile and related dev dependencies

--- Gulpfile.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
